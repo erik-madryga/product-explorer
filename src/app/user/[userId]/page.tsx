e4fdd230492ad968562b920852aece3255615516
Erik Madryga <94417988+erik-madryga@users.noreply.github.com>
2025-12-30 06:52:47 -0800

    fix(userPage): add await for params (#11)

--- src/app/user/[userId]/page.tsx
+++ src/app/user/[userId]/page.tsx
@@ -2,10 +2,9 @@ import UserCard from '../../../components/User/UserCard';
 import { getData } from '../../../lib/getData';
 import { USERS } from '../../../constants/strings';
 
-
-export default async function Page({ params }: { params: { userId: string } }) {
-  console.log("User userId from params:", params.userId);
-  const user = await getData(USERS+`/${params.userId}`);
+export default async function Page({ params }: { params: Promise<{ userId: string }> }) {
+  const resolvedParams = await params;
+  const user = await getData(USERS + `/${resolvedParams.userId}`);
   return (
     <div className="space-y-6">
       <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
