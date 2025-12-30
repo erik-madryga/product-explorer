26e5b4312739a3d9d75e413c4e1c934299faf4b8
erik-madryga <madryga7@gmail.com>
2025-12-30 06:40:45 -0800

    chore(cartIcon): delete extra

--- src/components/CartIcon.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { CIcon } from '@coreui/icons-react';
-import { cilCart } from '@coreui/icons';
-
-export function CartIcon({ className = "" }: { className?: string }) {
-  return <CIcon icon={cilCart} className={`w-6 h-6 ${className}`} />;
-}
